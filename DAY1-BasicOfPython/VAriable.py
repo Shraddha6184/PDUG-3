5ec1c33de3638ea7bc9b2ec3375f2f018b9b3611
Shraddha Shinde <144773567+Shraddha6184@users.noreply.github.com>
2024-08-07 10:24:55 +0000

    done

---
+++ DAY1-BasicOfPython/VAriable.py
@@ -0,0 +1,9 @@
+# input your data
+
+name=input("Enter your name ")
+age=int(input("Enter your age "))
+hobby=input("Enter your hobby ")
+
+# print your statement
+
+print("Myself",name,"I like",hobby,"and my age is",age,"years.")
